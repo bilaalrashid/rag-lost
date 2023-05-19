7a8144f278d6983ae3824e270edcbc3dcd6f59ad
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-19 22:38:53 +0100

    Add database code

---
+++ public/php/stores/DatabaseController.php
@@ -0,0 +1,21 @@
+<?php
+
+require "__connect.php";
+
+abstract class DatabaseController {
+
+	protected $connection;
+
+	public function __construct() {
+		$this->connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+		
+		if ($this->connection->connect_errno) {
+			echo "Failed to connect to MySQL: " . $connection->connect_error;
+		}
+	}
+
+	public function __destruct() {
+		$this->connection->close();
+	}
+
+}
