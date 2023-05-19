ca6af4c3bdc56729b423f7914322709d4838e06b
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-19 17:08:01 +0100

    Move position of zoom control

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,15 @@
-const map = L.map('map').setView([51.505, -0.09], 13);
+const initialCenter = [51.505, -0.09];
+const initialZoom = 13;
+
+const map = L.map('map', {
+  zoomControl: false,
+}).setView(initialCenter, initialZoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+L.control.zoom({
+  position: 'bottomleft'
 }).addTo(map);
