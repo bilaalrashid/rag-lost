1c6b896874fe34b0f1dd1dad37ca61a79abd2165
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-19 17:50:54 +0100

    Fetch data from API

--- public/js/map.js
+++ public/js/map.js
@@ -13,3 +13,21 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.control.zoom({
   position: 'bottomleft'
 }).addTo(map);
+
+const fetchData = async () => {
+  const response = await fetch('/api/teams.json');
+  const json = await response.json();
+  return json;
+}
+
+const updateMap = (data) => {
+  console.log(data);
+}
+
+const refreshData = async () => {
+  const data = await fetchData();
+  updateMap(data);
+}
+
+refreshData().catch();
+setInterval(refreshData, 10_000);
