adcfdf673fa368c5b85c41ca4b5ed9996f29c430
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-20 14:15:37 +0100

    Update site Config schema

--- public/php/models/Config.php
+++ public/php/models/Config.php
@@ -6,13 +6,24 @@
 class Config {
 
 	private $countdown_start;
+	private $start_location_latitude;
+	private $start_location_longitude;
+	private $donation_total;
+	private $donate_url;
 
 	/**
 	 * Constructor
-	 * @param DateTime $countdown_start    [The time that the countdown should start from]
+	 * @param DateTime $countdown_start           [The time that the countdown should start from]
+	 * @param float    $start_location_latitude   [The latitude of the start location]
+	 * @param float    $start_location_longitude  [The longitude of the start location]
+	 * @param float    $donation_total            [The total amount of money raised]
+	 * @param string   $donate_url                [The URL of the donation page]
 	 */
-	public function __construct(DateTime $countdown_start) {
+	public function __construct(DateTime $countdown_start, float $start_location_latitude, float $start_location_longitude, float $donation_total, string $donate_url) {
 		$this->countdown_start = $countdown_start;
+		$this->start_location_latitude = $start_location_latitude;
+		$this->start_location_longitude = $start_location_longitude;
+		$this->donation_total = $donation_total;
 	}
 
 	/**
@@ -21,6 +32,38 @@ class Config {
 	 */
 	public function getCountdownStart(): DateTime {
 		return $this->countdown_start;
+	}
+
+	/**
+	 * Gets the start location latitude
+	 * @return float [The latitude of the start location]
+	 */
+	public function getStartLocationLatitude(): float {
+		return $this->start_location_latitude;
+	}
+
+	/**
+	 * Gets the start location longitude
+	 * @return float [The longitude of the start location]
+	 */
+	public function getStartLocationLongitude(): float {
+		return $this->start_location_longitude;
+	}
+
+	/**
+	 * Gets the donation total
+	 * @return float [The total amount of money raised]
+	 */
+	public function getDonationTotal(): float {
+		return $this->donation_total;
+	}
+
+	/**
+	 * Gets the donate URL
+	 * @return string [The URL of the donation page]
+	 */
+	public function getDonateUrl(): string {
+		return $this->donate_url;
 	}
 
 }
--- public/php/stores/ConfigStore.php
+++ public/php/stores/ConfigStore.php
@@ -9,17 +9,21 @@ class ConfigStore extends DatabaseController {
 	/**
 	 * Edits the website config in the database.
    * 
-   * @param  string  $countdown_start [The time that the countdown should start from]
+   * @param  string  $countdown_start           [The time that the countdown should start from]
+	 * @param  float   $start_location_latitude   [The latitude of the start location]
+	 * @param  float   $start_location_longitude  [The longitude of the start location]
+	 * @param  float   $donation_total            [The total amount of money raised]
+	 * @param  string  $donate_url                [The URL of the donation page]
 	 */
-	public function editConfig(DateTime $countdown_start) {
+	public function editConfig(DateTime $countdown_start, float $start_location_latitude, float $start_location_longitude, float $donation_total, string $donate_url) {
 		$db = $this->connection;
 
 		$sql = 
     "UPDATE config 
-			SET countdown_start = ?";
+			SET countdown_start = ?, start_location_latitude = ?, start_location_longitude = ?, donation_total = ?, donate_url = ?";
 
 		$statement = $db->prepare($sql);
-		$statement->bind_param("s", $countdown_start);
+		$statement->bind_param("sddds", $countdown_start, $start_location_latitude, $start_location_longitude, $donation_total, $donate_url);
 
 		$statement->execute();
 		$result = $statement->get_result();
@@ -50,7 +54,7 @@ class ConfigStore extends DatabaseController {
 		if ($result->num_rows > 0) {
 			$row = $result->fetch_assoc();
 
-			$team = new Team($row["id"], $row["countdown_start"]);
+			$team = new Team($row["id"], $row["countdown_start"], $row["start_location_latitude"], $row["start_location_longitude"], $row["donation_total"], $row["donate_url"]);
 		}
 
 		$statement->close();
