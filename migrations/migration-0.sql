cc6136cf9cbd5ac5890b594daa207a202c9f3990
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-20 13:49:26 +0100

    Naughtily update the db schema

--- migrations/migration-0.sql
+++ migrations/migration-0.sql
@@ -1,14 +1,16 @@
 CREATE TABLE team (
-  `id` INTEGER PRIMARY KEY,
+  `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
   `team_name` TEXT NOT NULL,
   `members` TEXT NOT NULL,
   `team_description` TEXT NOT NULL,
   `donate_url` TEXT NOT NULL,
-  `pin_url` TEXT NOT NULL
+  `pin_url` TEXT NOT NULL,
+  `team_image_url` TEXT NOT NULL,
+  `team_color` TEXT NOT NULL
 );
 
 CREATE TABLE location_update (
-  `id` INTEGER PRIMARY KEY,
+  `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
   `latitude` REAL NOT NULL,
   `longitude` REAL NOT NULL,
   `update_message` TEXT NOT NULL,
@@ -18,12 +20,18 @@ CREATE TABLE location_update (
 );
 
 CREATE TABLE config (
-  `id` INTEGER PRIMARY KEY,
-  `countdown_start` DATETIME NOT NULL
+  `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `countdown_start` DATETIME NOT NULL,
+  `start_location_latitude` REAL NOT NULL,
+  `start_location_longitude` REAL NOT NULL,
+  `donation_total` REAL,
+  `donate_url` TEXT
 );
 
 CREATE TABLE version (
   `version` VARCHAR(255) NOT NULL
 );
 
-INSERT INTO `Version` (`version`) VALUES ('1.0.0');
+INSERT INTO `config` (`countdown_start`, `start_location_latitude`, `start_location_longitude`, `donation_total`, `donate_url`) VALUES (NOW(), 0.0, 0.0, 0, '');
+
+INSERT INTO `version` (`version`) VALUES ('1.0.0');
