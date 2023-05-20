661760b144c1acf73db39ebc8bcd2275021d4d35
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-20 16:50:35 +0100

    Add base UI to admin panel

--- public/admin/index.php
+++ public/admin/index.php
@@ -12,7 +12,20 @@
     <?php require_once "../tpl/admin/header.php"; ?>
 		<section>
 			<div class="wrapper">
-
+				<p>Welcome to the RAG Lost admin panel! This is where you can create, edit and manage details of teams, and update their location on the tracker map. You can also edit global settings for the site here too. For anything else, you will probably need to touch the code, but hopefully it isn't <i>too</i> scary.</p>
+				<div>
+					<h2>General Settings</h2>
+					<a href="/admin/settings">Edit here</a>
+				</div>
+				<div>
+					<h2>Teams</h2>
+					<hr />
+					<div>
+						<h3>Team Name</h3>
+						<span><a href="/team/edit">Edit</a> | <a href="/team/location-update">Update Location</a></span>
+					</div>
+					<hr />
+				</div>
 			</div>
 		</section>
 	</body>
