d9c0bdd195abde57f238d5ed44d011a4d89fef81
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-20 17:34:01 +0100

    Add UI for add team

---
+++ public/php/controllers/AddTeamController.php
@@ -0,0 +1,17 @@
+<?php
+
+class AddTeamController {
+	
+	public function main() {
+		return null;
+	}
+
+	public function post() {
+		return null;
+	}
+
+	public function get() {
+		return null;
+	}
+
+}
