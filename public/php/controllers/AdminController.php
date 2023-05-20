af7913db87933239155db06f4c5ad1006386a45b
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-20 16:27:42 +0100

    Style admin header

---
+++ public/php/controllers/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+class AdminController {
+	
+	public function main() {
+		return null;
+	}
+
+	public function post() {
+		return null;
+	}
+
+	public function get() {
+		return null;
+	}
+
+}
---
+++ public/tpl/admin/header.php
@@ -0,0 +1,4 @@
+<header>
+  <img src="/img/rag.png" alt="Southampton RAG logo" />
+  <a href="/admin/logout">Logout</a>
+</header>
