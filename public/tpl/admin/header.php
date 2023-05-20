f5d094f8fd8c9e59f2cbf68265b9367eeaa34074
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-20 17:35:45 +0100

    Make header logo link to home

--- public/tpl/admin/header.php
+++ public/tpl/admin/header.php
@@ -1,4 +1,4 @@
 <header>
-  <img src="/img/rag.png" alt="Southampton RAG logo" />
+  <a href="/admin"><img src="/img/rag.png" alt="Southampton RAG logo" /></a>
   <a href="/admin/logout">Logout</a>
 </header>
