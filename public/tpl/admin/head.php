c8b4dc1441ee86dab13a366a232dd8e32a82c626
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-20 16:54:58 +0100

    Update admin panel message

--- public/tpl/admin/head.php
+++ public/tpl/admin/head.php
@@ -1,4 +1,5 @@
 <head>
+  <meta charset="UTF-8">
   <title><?php echo $title; ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="/css/admin.css" rel="stylesheet" type="text/css">
