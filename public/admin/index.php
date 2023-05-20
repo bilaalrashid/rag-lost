4acad5f6f42e6f9c3df8ddbf1a2fba436ae074e7
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-20 16:27:42 +0100

    Style admin header

---
+++ public/admin/index.php
@@ -0,0 +1,19 @@
+<?php
+	set_include_path(__DIR__."/../php/");
+	require_once "files/autoload.php";
+	$class = "AdminController";
+	$title = "Admin";
+	require_once "files/controller.php";
+?>
+<!DOCTYPE html>
+<html>
+  <?php require_once "../tpl/admin/head.php"; ?>
+	<body>
+    <?php require_once "../tpl/admin/header.php"; ?>
+		<section>
+			<div class="wrapper">
+
+			</div>
+		</section>
+	</body>
+</html>
---
+++ public/tpl/admin/head.php
@@ -0,0 +1,5 @@
+<head>
+  <title><?php echo $title; ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="/css/admin.css" rel="stylesheet" type="text/css">
+</head>
