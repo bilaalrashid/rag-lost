3e52da776eaf895d61a0ff6e4a3d35d9cfd9948b
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-21 14:48:10 +0100

    Add location update page

---
+++ public/admin/location-update/view/index.php
@@ -0,0 +1,19 @@
+<?php
+	set_include_path(__DIR__."/../../../php/");
+	require_once "files/autoload.php";
+	$class = "ViewLocationUpdateController";
+	$title = "Location Updates";
+	require_once "files/controller.php";
+?>
+<!DOCTYPE html>
+<html>
+  <?php require_once "../../../tpl/admin/head.php"; ?>
+	<body>
+    <?php require_once "../../../tpl/admin/header.php"; ?>
+		<section>
+			<div class="wrapper">
+        <h1>Location Updates</h1>
+			</div>
+		</section>
+	</body>
+</html>
