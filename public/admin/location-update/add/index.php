db526e1f98ee4da1c25afa6b32f9e2c39878b8d9
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-21 14:54:32 +0100

    Add location update page

--- public/admin/location-update/add/index.php
+++ public/admin/location-update/add/index.php
@@ -1,8 +1,8 @@
 <?php
 	set_include_path(__DIR__."/../../../php/");
 	require_once "files/autoload.php";
-	$class = "ViewLocationUpdateController";
-	$title = "Location Updates";
+	$class = "AddLocationUpdateController";
+	$title = "Add Location Update";
 	require_once "files/controller.php";
 ?>
 <!DOCTYPE html>
@@ -12,8 +12,7 @@
     <?php require_once "../../../tpl/admin/header.php"; ?>
 		<section>
 			<div class="wrapper">
-        <h1>Location Updates</h1>
-        <p><a href="/admin/location-update/add?teamID=<?php echo $_GET["teamID"]; ?>">Add New Update</a></p>
+        <h1>Add Location Update</h1>
 			</div>
 		</section>
 	</body>
