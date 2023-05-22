caa5b01f3ab73926134517748e51378b09d47730
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-23 00:25:59 +0100

    Add charity name to config model

--- public/php/stores/ConfigStore.php
+++ public/php/stores/ConfigStore.php
@@ -14,17 +14,18 @@ class ConfigStore extends DatabaseController {
 	 * @param  float   $start_location_longitude  [The longitude of the start location]
 	 * @param  float   $donation_total            [The total amount of money raised]
 	 * @param  string  $donate_url                [The URL of the donation page]
+	 * @param  string  $charity_name              [The name of the charity fundraising for]
 	 */
-	public function editConfig(DateTimeInterface $countdown_start, float $start_location_latitude, float $start_location_longitude, float $donation_total, string $donate_url) {
+	public function editConfig(DateTimeInterface $countdown_start, float $start_location_latitude, float $start_location_longitude, float $donation_total, string $donate_url, string $charity_name) {
 		$db = $this->connection;
 
 		$sql = 
     "UPDATE config 
-			SET countdown_start = ?, start_location_latitude = ?, start_location_longitude = ?, donation_total = ?, donate_url = ?";
+			SET countdown_start = ?, start_location_latitude = ?, start_location_longitude = ?, donation_total = ?, donate_url = ?, charity_name = ?";
 
 		$statement = $db->prepare($sql);
 		$formatted_countdown_start = $countdown_start->format("Y-m-d H:i:s");
-		$statement->bind_param("sddds", $formatted_countdown_start, $start_location_latitude, $start_location_longitude, $donation_total, $donate_url);
+		$statement->bind_param("sdddss", $formatted_countdown_start, $start_location_latitude, $start_location_longitude, $donation_total, $donate_url, $charity_name);
 
 		$statement->execute();
 		$result = $statement->get_result();
@@ -55,7 +56,7 @@ class ConfigStore extends DatabaseController {
 		if ($result->num_rows > 0) {
 			$row = $result->fetch_assoc();
 
-			$config = new Config(date_create_immutable_from_format("Y-m-d H:i:s", $row["countdown_start"]), $row["start_location_latitude"], $row["start_location_longitude"], $row["donation_total"], $row["donate_url"]);
+			$config = new Config(date_create_immutable_from_format("Y-m-d H:i:s", $row["countdown_start"]), $row["start_location_latitude"], $row["start_location_longitude"], $row["donation_total"], $row["donate_url"], $row["charity_name"]);
 		}
 
 		$statement->close();
