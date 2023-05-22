ecba4eb725d45988c364eb8cb788a5a2723afcb6
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-22 23:59:02 +0100

    Implement redirecting from holding page

---
+++ public/holding/index.php
@@ -0,0 +1,18 @@
+<?php
+	set_include_path(__DIR__."/../php/");
+	require_once "files/autoload.php";
+	$class = "TrackerHoldingController";
+	require_once "files/controller.php";
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>RAG Lost</title>
+    <link rel="stylesheet" href="/css/holding.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <h1>Starting Soon</h1>
+  </body>
+</html>
---
+++ public/php/controllers/TrackerHoldingController.php
@@ -0,0 +1,30 @@
+<?php
+
+class TrackerHoldingController {
+	
+	public function main() {
+		$store = new ConfigStore();
+		$config = $store->getConfig();
+
+    $now = new DateTimeImmutable();
+    $countdown_start = $config->getCountdownStart();
+    if ($now > $countdown_start) {
+      $host = $_SERVER["HTTP_HOST"];
+      header("Location: http://{$host}/");
+      exit();
+
+      return null;
+    }
+
+		return $config;
+	}
+
+	public function post() {
+		return null;
+	}
+
+	public function get() {
+		return null;
+	}
+
+}
