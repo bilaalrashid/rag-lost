1475f8643eb39d4b7f91fe174e13f4feda51ee80
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-23 00:40:55 +0100

    Use refresh interval of 1 minute

--- public/js/constants.js
+++ public/js/constants.js
@@ -1,6 +1,6 @@
 const initialCenter = [51.505, -0.09];
 const initialZoom = 13;
-const refreshInterval = 1000 * 60 * 15; // 15 minutes
+const refreshInterval = 1000 * 60 * 1; // 1 minute
 
 const map = L.map('map', {
   zoomControl: false,
