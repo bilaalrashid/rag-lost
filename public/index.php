7a5affc755f0862e352a7ce66d3eed7a889da4fc
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-23 00:55:06 +0100

    Add favicons

--- public/index.php
+++ public/index.php
@@ -12,6 +12,22 @@
     <link rel="stylesheet" href="/css/index.css">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="msapplication-TileColor" content="#D5282A">
+    <meta name="msapplication-TileImage" content="/img/favicon/favicon-144.png">
+    <link rel="apple-touch-icon-precomposed" sizes="192x192" href="/img/favicon/favicon-192.png">
+    <link rel="apple-touch-icon-precomposed" sizes="180x180" href="/img/favicon/favicon-180.png">
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/img/favicon/favicon-152.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/img/favicon/favicon-144.png">
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/img/favicon/favicon-120.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/img/favicon/favicon-114.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/img/favicon/favicon-72.png">
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/img/favicon/favicon-60.png">
+    <link rel="apple-touch-icon-precomposed" href="/img/favicon/favicon-57.png">
+    <link rel="icon" href="/img/favicon/favicon-96.png" sizes="96x96">
+    <link rel="icon" href="/img/favicon/favicon-48.png" sizes="48x48">
+    <link rel="icon" href="/img/favicon/favicon-32.png" sizes="32x32">
+    <link rel="icon" href="/img/favicon/favicon-16.png" sizes="16x16">
+    <link href="/favicon.ico" rel="shortcut icon">
   </head>
   <body>
     <div class="vertical-layout">
