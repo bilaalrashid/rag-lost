fe7fb013f0cf71996b1c49540f580cece35224ba
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-23 00:14:04 +0100

    Add content to holding page

--- public/holding/index.php
+++ public/holding/index.php
@@ -8,11 +8,21 @@
 <html>
   <head>
     <meta charset="UTF-8">
-    <title>RAG Lost</title>
+    <title>RAG Lost | Starting Soon</title>
     <link rel="stylesheet" href="/css/holding.css">
+    <meta http-equiv="refresh" content="60">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <body>
-    <h1>Starting Soon</h1>
+    <div class="wrapper">
+      <img src="/img/rag.png" alt="Southampton RAG Logo" class="logo" />
+      <h1>RAG Lost Map Tracker</h1>
+      <h2>12 hours to navigate back to Southampton from a mystery location</h2>
+      <h3>Raising Money for Young Minds</h3>
+      <h3>Event Starting Soon, Check Back Later</h3>
+      <p class="donate-container">
+        <a href="<?php echo $model->getDonateURL(); ?>" target="_blank" class="donate">Donate Online</a>
+      </p>
+    </div>
   </body>
 </html>
