d7cffd2b1f67bc43ed2e3f9e3d64bf4c4a98bb3e
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-22 23:55:58 +0100

    Redirect to holding page if event hasn't started yet

--- public/php/controllers/TrackerController.php
+++ public/php/controllers/TrackerController.php
@@ -5,6 +5,17 @@ class TrackerController {
 	public function main() {
 		$store = new ConfigStore();
 		$config = $store->getConfig();
+
+    $now = new DateTimeImmutable();
+    $countdown_start = $config->getCountdownStart();
+    if ($now < $countdown_start) {
+      $host = $_SERVER["HTTP_HOST"];
+      header("Location: http://{$host}/holding/");
+      exit();
+
+      return null;
+    }
+
 		return $config;
 	}
 
