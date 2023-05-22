8ad2b8585390940ae0bea545441167cba00e477c
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-23 00:58:21 +0100

    Style admin login page

--- public/admin/login/index.php
+++ public/admin/login/index.php
@@ -7,18 +7,18 @@
 ?>
 <!DOCTYPE html>
 <html>
-  <head></head>
+	<?php require_once "../../tpl/admin/head.php"; ?>
 	<body>
-		<header></header>
 		<section>
 			<div class="wrapper">
 				<div class="login">
-					<h2><?php echo $title; ?></h2>
+					<img src="/img/rag.png" alt="Southampton RAG Logo" class="logo">
+					<h1>Admin Login</h1>
 					<form method="POST">
-						<div class="input-container">
+						<div class="form-row">
 							<input type="text" name="username" class="user" placeholder="Username" required>
 						</div>
-						<div class="input-container">
+						<div class="form-row">
 							<input type="password" name="password" class="pass" placeholder="Password" required>
 						</div>
 						<button type="submit" class="login-submit">Login</button>
@@ -31,6 +31,5 @@
 				<?php } ?>
 			</div>
 		</section>
-		<footer></footer>
 	</body>
 </html>
