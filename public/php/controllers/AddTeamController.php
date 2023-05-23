35cd41165a366d3e6ea8c60865631b55eb48cf5f
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-23 18:22:19 +0100

    Add file uploading to the add team form

--- public/php/controllers/AddTeamController.php
+++ public/php/controllers/AddTeamController.php
@@ -7,17 +7,20 @@ class AddTeamController {
 	}
 
 	public function post() {
+		$upload_directory =  __DIR__ . "/../../img/uploads";
+		$original_team_image_name = FileUploadController::validate_single_image('team_image', $upload_directory);
+
 		$team_name = $_POST["team_name"];
 		$members = $_POST["members"];
 		$description = $_POST["description"];
 		$donate_url = $_POST["donate_url"];
-		$pin_url = '';
-		$team_image_url = $_POST["team_image_url"];
-		$team_color = '';
 
-		if (!empty($team_name) && !empty($donate_url)) {
+		if (!empty($team_name) && !empty($donate_url) && !empty($original_team_image_name)) {
+			$pin_url = '';
+			$team_color = '';
+
 			$store = new TeamStore();
-			$store->addTeam($team_name, $members ?: '', $description ?: '', $donate_url, $pin_url, $team_image_url ?: '', $team_color);
+			$store->addTeam($team_name, $members ?: '', $description ?: '', $donate_url, $pin_url, "/img/uploads/" . $original_team_image_name, $team_color);
 
 			$host = $_SERVER["HTTP_HOST"];
 			header("Location: http://{$host}/admin/");
---
+++ public/php/helpers/FIleUploadController.php
@@ -0,0 +1,68 @@
+<?php
+
+class FileUploadController {
+
+  /**
+   * Validates an uploaded file is an image.
+   * 
+   * @param  string  $form_name         [The name of the form field the file was uploaded to]
+   * @param  string  $target_directory  [The directory to move the file to]
+   * @return ?string                    [The name of the uploaded file, or null if the file was invalid]
+   */
+  static function validate_single_image(string $form_name, string $target_directory): ?string {
+    $file = $_FILES[$form_name];
+
+    // Check we've uploaded a file
+    if (!empty($file) && $file['error'] == UPLOAD_ERR_OK) {
+      $tmp_file_name = $file['tmp_name'];
+
+      // Be sure we're definitely dealing with an upload
+      if (is_uploaded_file($tmp_file_name) === false) {
+        return null;
+      }
+
+      // Don't trust the user-supplied MIME type, read it directly from the file
+      $finfo = finfo_open(FILEINFO_MIME_TYPE);
+      $mime_type = finfo_file($finfo, $tmp_file_name);
+      finfo_close($finfo);
+
+      $valid_mime_types = array("image/jpeg" => "jpg", "image/png" => "png", "image/gif" => "gif");
+
+      // Check MIME type
+      if (!array_key_exists($mime_type, $valid_mime_types)) {
+        return null;
+      }
+
+      $file_size = filesize($tmp_file_name);
+      $max_file_size = 5_000_000; // 5MB
+
+      // Check if file is empty
+      if ($file_size == 0) {
+        return null;
+      }
+
+      // Check file is less than maximum size
+      if ($file_size > $max_file_size) {
+        return null;
+      }
+
+      $ext = $valid_mime_types[$mime_type];
+      $file_name = round(microtime(true)) . mt_rand() . "." . $ext;
+      $target_destination = $target_directory . "/" . $file_name;
+
+      // Create target directory if it doesn't exist
+      if (!file_exists($target_directory)) {
+        mkdir($target_directory, 0775, true);
+      }
+
+      // Move file from temp directory
+      $success = move_uploaded_file($tmp_file_name, $target_destination);
+      if ($success) {
+        return $file_name;
+      }
+    }
+
+    return null;
+  }
+
+}
