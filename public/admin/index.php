1a32a14eccb10d4e6592fa681c5d7becb7b0d65f
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-05-24 01:33:40 +0100

    Use h1 for team name

--- public/admin/index.php
+++ public/admin/index.php
@@ -42,7 +42,7 @@
 											<img src="<?php echo $team->getTeamImageURL(); ?>" style="border-color: <?php echo $team->getTeamColor(); ?>" />
 										</td>
 										<td>
-											<h2><?php echo $team->getTeamName(); ?></h2>
+											<h1><?php echo $team->getTeamName(); ?></h1>
 											<p><?php echo $team->getMembers(); ?></p>
 											<p><a href="/admin/team/edit?id=<?php echo $team->getID(); ?>" class="link-button">Edit Details</a> | <a href="/admin/location-update/view?teamID=<?php echo $team->getID(); ?>" class="link-button">Location Updates</a></p>
 										</td>
