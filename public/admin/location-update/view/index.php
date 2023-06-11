a312d188a24327aa19ea24992c78360e28bdbb55
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-06-11 22:41:47 +0100

    Update location updates view page

--- public/admin/location-update/view/index.php
+++ public/admin/location-update/view/index.php
@@ -13,7 +13,8 @@
 		<section>
 			<div class="wrapper">
 				<p><a href="/admin">Home</a> > Location Updates</p>
-        <h1>Location Updates &#8226; <?php echo $get[0]->getTeamName(); ?></h1>
+        <h1>Location Updates</h1>
+				<h2><?php echo $get[0]->getTeamName(); ?></h2>
 				<p>Add and edit location updates for the team here. NOTE: You do NOT need to add the start location, that is already included for you on the tracker.</p>
         <p><a href="/admin/location-update/add?teamID=<?php echo $_GET["teamID"]; ?>" class="link-button">Add New Update</a></p>
 				<hr />
@@ -24,9 +25,9 @@
 						foreach ($get[1] as $update) {
 				?>
 					<div class="location-update-list-item">
-						<h1><?php echo $update->getLocationName(); ?></h1>
-						<p><?php echo $update->getUpdateTimestamp()->format("H:i, d/m/Y"); ?></p>
+						<h3><?php echo $update->getLocationName(); ?></h3>
 						<p class="message">"<?php echo $update->getUpdateMessage(); ?>"</p>
+						<p><?php echo $update->getUpdateTimestamp()->format("H:i, d/m/Y"); ?></p>
 						<p><a href="/admin/location-update/edit?id=<?php echo $update->getID(); ?>&teamID=<?php echo $_GET["teamID"]; ?>" class="link-button">Edit</a></p>
 					</div>
 					<hr />
